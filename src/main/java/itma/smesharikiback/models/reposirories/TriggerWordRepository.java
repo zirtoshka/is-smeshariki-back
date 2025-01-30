b12c45bb311e2d0fd4832ff6d47faf9b3a444138
Vydra <Vydra@lanit.ru>
2025-01-31 02:23:04 +0300

    added trigger words

--- src/main/java/itma/smesharikiback/models/reposirories/TriggerWordRepository.java
+++ src/main/java/itma/smesharikiback/models/reposirories/TriggerWordRepository.java
@@ -2,9 +2,10 @@ package itma.smesharikiback.models.reposirories;
 
 import itma.smesharikiback.models.TriggerWord;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.List;
 
-public interface TriggerWordRepository extends JpaRepository<TriggerWord, Long> {
+public interface TriggerWordRepository extends JpaRepository<TriggerWord, Long>, JpaSpecificationExecutor<TriggerWord> {
     List<TriggerWord> findByPropensityId(Long propensityId);
 }
---
+++ src/main/java/itma/smesharikiback/response/TriggerWordResponse.java
@@ -0,0 +1,12 @@
+package itma.smesharikiback.response;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+@Data
+@Accessors(chain = true)
+public class TriggerWordResponse {
+    private Long id;
+    private String word;
+    private Long propensity;
+}
